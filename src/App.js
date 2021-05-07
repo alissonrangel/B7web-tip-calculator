df73ecb1846e0cd4c4adf2951cd9daeb10fa1a2d
Alisson Rangel <alisson.rangel@hotmail.com>
2021-05-06 23:19:25 -0300

    Initial commit, making a tip calculator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  margin: 10px;
+`
+const Title = styled.h1`
+  font-size: 26px;
+  margin-bottom: 10px;
+`
+const Input = styled.input`
+  width: 400px;
+  height: 30px;
+  padding: 10px;
+  display: block;
+  margin-top: 10px;
+` 
 
 function App() {
+
+  const [conta, setConta] = useState('');
+  const [porcentagem, setPorcentagem] = useState('10');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Container>
+     <Title>Calculadora de Gorjetas</Title>
+     <label>Quanto deu a conta?</label>
+     <Input type="number" value={conta} onChange={(e) => setConta(e.target.value)} ></Input> 
+     <br/>
+     <br/>
+     <label>Qual a porcentagem de gorjeta?</label>
+     <Input type="number" value={porcentagem} onChange={(e) => setPorcentagem(e.target.value)} ></Input> 
+     <hr/>
+
+     {  conta && 
+        <div>
+          <p>Sub-total: R${conta}</p>
+          <p>Gorjeta(%{porcentagem}): R$ {parseFloat(conta * (porcentagem/100)).toFixed(2)}</p>
+          <h3>Total: R$ {parseFloat((conta * (porcentagem/100 + 1))).toFixed(2)}</h3>
+        </div>
+     }
+   </Container>
   );
 }
 
